01f069bb5f8e73cd2ace8dbb76ece78e50f35198
douglasdl <douglas_san@hotmail.com>
2024-09-12 19:02:42 -0300

    feat: finish part 1

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-let goal = {
-  value: 'Ler um livro por mês',
-  checked: false
+// Hello World!
+console.log("Hello World!")
+
+
+// variables - let
+let messages = "Hello World!"
+
+
+// variables - const
+const menssage = "Hello, me"
+{
+    const menssage = "Hello, Douglas!"
+    console.log(menssage)
 }
-let goals = [
-  goal,
-  {
-    value: 'Caminhar 20 minutos todos os dias',
-    checked: false
-  }
+console.log(menssage);
+
+
+// arrays
+let goals00 = ['Douglas', 'hello']
+let goals01 = [2, 'Douglas']
+// concatenate values
+console.log(goals00[1] + ", " + goals00[0]) 
+
+let goals02 = [
+    goals01,
+    {
+        value: 'caminhar 20 minutos todos os dias',
+        checked: false
+    }
 ]
+console.log(goals02[1].value);
 
-function createGoal() {
-  console.log(goals[0].value)
+// objects
+let goals = {
+    value: 'ler um livro por mês',
+    address: 2,
+    checked: true,
+    isChecked: () => {
+        console.log(info)
+    }
 }
+console.log(goals.value);
 
-createGoal()
+// function // arrow function
+const createGoal = () => {}
 
+// named function
+function createGoals() {}
